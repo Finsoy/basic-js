be7ec10d663e9bbb6f9ed2a4308e327e48b4b130
Finsoy <vadimka.cry.00@gmail.com>
2021-02-27 12:33:45 +0300

    feat: add transform(arr) func

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArr = [];
+  if (Array.isArray(arr)) {
+    for (let i = 0; i <= arr.length - 1; i++) {
+      if (arr[i] === '--discard-next') {
+      } else if (arr[i - 1] === '--discard-next') {
+        newArr.splice(arr.indexOf(arr[i]), 1);
+      } else if (arr[i] === '--discard-prev') {
+        if (i === 0) {
+        } else {
+          newArr.splice(arr.indexOf(arr[i - 1]), 1);
+        }
+      } else if (arr[i] === '--double-next') {
+        if (i === arr.length - 1) {
+        } else {
+          newArr.push(arr[i + 1]);
+        }
+      } else if (arr[i] === '--double-prev') {
+        if (i === 0) {
+        } else {
+          if (arr[i - 2] === '--discard-next') {
+          } else {
+            newArr.push(arr[i - 1]);
+          }
+        }
+      } else newArr.push(arr[i]);
+    }
+  } else throw Error('Error');
+  return newArr;
 };
