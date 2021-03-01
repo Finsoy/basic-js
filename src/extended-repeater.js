6c7b58f603444dbeab6ba6a7af9b862e3f34785a
Finsoy <vadimka.cry.00@gmail.com>
2021-03-01 10:41:32 +0200

    feat: add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,13 +2,51 @@ const CustomError = require('../extensions/custom-error');
 
 module.exports = function repeater(str, options) {
   let string = '';
+  let addition = '';
   if (!options.separator) {
     options.separator = '+';
   }
+  if (!options.additionSeparator) {
+    options.additionSeparator = '|';
+  }
+  if (options.repeatTimes) {
+    if (options.repeatTimes === 1 && options.additionRepeatTimes === 1) {
+      if (options.addition) {
+        string = `${str}${options.addition}`;
+      } else {
+        string = `${str}`;
+      }
+    } else {
+      for (let i = 1; i <= options.repeatTimes; i++) {
+        addition = '';
+        if (options.additionRepeatTimes) {
+          for (let j = 1; j <= options.additionRepeatTimes; j++) {
+            if (j === options.additionRepeatTimes) {
+              addition += `${options.addition}`;
+            } else {
+              addition += `${options.addition}${options.additionSeparator}`;
+            }
+          }
+        } else {
+          if (options.addition) {
+            addition = `${options.addition}`;
+          }
+        }
 
-  if (options.repeatTimes > 1) {
-    string = `${str} `;
+        if (i === options.repeatTimes) {
+          string += `${str}${addition}`;
+        } else {
+          string += `${str}${addition}${options.separator}`;
+        }
+      }
+    }
+  } else {
+    if (options.addition) {
+      string = `${str}${options.addition}`;
+    } else {
+      string = `${str}`;
+    }
   }
 
-  return string.repeat(options.repeatTimes).split(' ').join(options.separator);
+  return string;
 };
